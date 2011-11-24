f62a270c1ba3494fd24a777272a468e0710d3185
Reimer Behrends <behrends@gmail.com>
2011-11-24 22:39:23 +0100

    Added condition variable example.

--- example.cc
+++ example.cc
@@ -13,6 +13,9 @@ struct ThreadInfo {
 
 #include "tlsize.cc"
 
+ConditionVariable *cond;
+Lock *lock;
+
 class ThreadBody : public ThreadAction {
 public:
   virtual void main(Thread *self) {
@@ -21,17 +24,24 @@ public:
     for (i=0; i<arg; i++)
       self->memory().counter++;
     self->info().result = self->memory().counter;
+    lock->lock(); cond->signal(); lock->unlock();
   }
 };
 
 int main() {
   Thread *t1, *t2;
   ThreadBody body;
+  lock = new Lock();
+  cond = new ConditionVariable(lock);
+  lock->lock();
   t1 = new Thread();
   t2 = new Thread();
   t1->info().arg =
-  t2->info().arg = 1000000000;
+  t2->info().arg = 100000000;
   t1->run(body); t2->run(body);
+  cond->wait();  cond->wait();
   t1->wait();    t2->wait();
+  lock->unlock();
   std::cout << "Result: " << t1->info().result + t2->info().result << "\n";
+  delete t1; delete t2;
 }
