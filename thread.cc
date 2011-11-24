266d1dd65a831658859095471b3c0b5beb4accfa
Reimer Behrends <behrends@gmail.com>
2011-11-24 23:50:17 +0100

    Added comments to thread implementation.

--- thread.cc
+++ thread.cc
@@ -71,17 +71,23 @@ void* Thread::operator new(size_t size) {
   void *addr;
   Thread *result;
   size_t pagesize = getpagesize();
+  // Thread-local storage area is rounded up the next highest multiple
+  // of the page size.
   size_t tlssize = (sizeof(Thread) +
     threadLocalDataSize() + threadInfoSize() + pagesize - 1)
     & ~(pagesize -1);
+  // Allocate twice the needed memory with anonymous mmap().
   addr = mmap(0, 2 * STACK_AREA_SIZE, PROT_READ|PROT_WRITE,
     MAP_PRIVATE|MAP_ANONYMOUS, -1 , 0);
+  // Find the smallest multiple of STACK_AREA_SIZE within the area.
   result = THREADP((((uintptr_t) addr) + (STACK_AREA_SIZE-1))
     & STACK_AREA_MASK);
+  // Unmap all memory before and after it.
   munmap(addr, CHARP(result)-CHARP(addr));
   munmap(CHARP(result)+STACK_AREA_SIZE,
     CHARP(addr)-CHARP(result)+STACK_AREA_SIZE);
-  /* generate a stack overflow protection area */
+  // Generate a stack overflow protection area and record
+  // the size and start of the actual stack.
 #ifdef STACK_GROWS_UP
   mprotect(CHARP(result) + STACK_AREA_SIZE - tlssize - pagesize,
     pagesize, PROT_NONE);
@@ -106,6 +112,9 @@ static void *dispatchThread(void *arg) {
 void Thread::run(ThreadAction &body) {
   pthread_attr_t attr;
   pthread_attr_init(&attr);
+  // Regardless of which direction the stack grows, the
+  // pthread_attr_setstack() call requires a pointer to the
+  // start of it.
   pthread_attr_setstack(&attr, stack_bottom, stack_size);
   pthread_create(&descriptor, &attr, dispatchThread, &body);
 }
@@ -122,6 +131,9 @@ void Thread::wait() {
 }
 
 void ThreadGrowMainStack() {
+  // This function makes sure that all pages on the main stack get paged
+  // in in the proper order; some operating systems will cause a page fault
+  // and crash if stack area allocation skips too many pages.
   char *tls = reinterpret_cast<char *>(Thread::current());
   size_t pagesize = getpagesize();
   char *p = CHARP(alloca(pagesize/2));
@@ -135,6 +147,7 @@ void ThreadGrowMainStack() {
     *p = '\0'; /* touch page */
     p = CHARP(alloca(pagesize/2));
   }
+  // And finally, initialize the thread-local storage.
   Thread::current()->Thread0();
 }
 
